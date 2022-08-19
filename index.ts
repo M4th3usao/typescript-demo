cbaaf3c05c2212e616802459900a3eea4c5dc178
M4th3usao <matheus.rico17@gmail.com>
2022-08-19 20:48:03 -0300

    2 files modified

--- index.ts
+++ index.ts
@@ -10,8 +10,17 @@ let counter: number = 0;
 
 let intervaloId = setInterval(() => {
   counter++;
-  document.getElementById('app.counter')
-  .innerHTML = counter.toString();
+  document.getElementById('app.counter').innerHTML = counter.toString();
 }, 1000);
+
+let descanso = setInterval(() => {
+  descanso++;
+  document.getElementById('app.descanso').innerHTML = descanso.toString();
+}, 500);
+
+let exercicio = setInterval(() => {
+  exercicio++;
+  document.getElementById('app.exercicio').innerHTML = descanso.toString();
+}, 500);
 
 document.getElementById('app.title').innerHTML = title;
