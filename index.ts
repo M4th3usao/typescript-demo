7f8c1576513d448aefbc99cce35a47200b0976a1
M4th3usao <matheus.rico17@gmail.com>
2022-08-19 19:34:43 -0300

    1 file modified, 1 added

--- index.ts
+++ index.ts
@@ -4,3 +4,9 @@ import './style.css';
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+console.log('Olá, mundo');
+
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
